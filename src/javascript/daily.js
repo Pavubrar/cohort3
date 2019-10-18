ba21b5a338a25a1b6a63fa751ade2d79a0b05d0b
Pavubrar <56096198+Pavubrar@users.noreply.github.com>
2019-10-18 11:09:44 -0700

    update daily.js

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -1,14 +1,102 @@
+// 2019-10-16 
 const functions = {
 
-    makeEmailObject: obj =>{
+    sortAscending: arr => {
+        arr.sort((a,b) => a-b);
+        return arr;
+    },
+
+    sortDescending: arr => {
+    
+  /*let compareFunction = function(a, b) {
+    return data[b][key] - data[a][key];
+  }*/
+  
+       arr.sort((a,b) => b-a);
+        return arr;
+    },
+
+    filterFunction: arr => {
+        const newarr = arr.filter (currValue => currValue>2);
+        return newarr;
+    },
+    reduceFunction: arr => {
+        const result = arr.reduce((acc, elem) => acc + elem);
+        return result;
+    },
+       
+    mapFunction: arr => {
+        const newArr = arr.map(elem => elem*5)
+        return newArr;
+    },
+    sliceFunction: arr => {
+        let newArr = arr.slice(1);
+        return newArr;
+    },
+    spliceFunction: arr => {
+        arr.splice(1,0,"splice");
+        return arr;
+    },
+
+    forEachFunction: arr => {
+        let elemlist =[];
+        arr.forEach(function(arrElem) {
+            elemlist.push(arrElem);
+
+        });
+        return elemlist;
+    },
+    forInLoop: obj => {
+        let propertyList =[];
+        for (let prop in obj) {
+            propertyList.push(obj[prop]);
+        }
+return propertyList;
+
+    },
+
+    forOfLoop: arr => {
+        let elemlist =[];
+        for (const elem of arr) {
+            elemlist.push(elem);
+        }
+        return elemlist;
+    },
+    doWhile: countUpto =>{
+        let countArr =[];
+        let i =1;
+        do{
+            countArr.push(i);
+            i++;
+        } while (i<= countUpto);
+        return countArr;
+    },
+    whileLoop: str => {
+        let newstr = "",
+         i = str.length;
+        while(i>0) {
+            newstr += str.substring(i - 1 , i)
+            i--;
+        }
+        return newstr;
+    },
+basicForLoop: (smallNum,largeNum) => {
+    let sum =0;
+for (let i= smallNum; i<=largeNum; i++){
+sum+=i;
+}
+return sum;
+},
+// OCT 11
+   makeEmailObject: obj =>{
         let first= obj.fname.toLowerCase();
         let last = obj.lname.toLowerCase();
     
      return `${first}.${last}@evolveu.ca`;
     }, 
     
-    
-    assertEqual: (in1,in2) => {
+    // OCT &
+  assertEqual: (in1,in2) => {
     
        
             const message ="*** the two values are not same:" + "\n";
@@ -26,13 +114,14 @@ const functions = {
     
 
 
-   makeEmailArr: (name) =>{
+   makeEmailArr: (name) => {
        let first= name[0].toLowerCase();
        let last = name[1].toLowerCase();
    
     return `${first}.${last}@evolveu.ca`;
    },
+};
   
- };
+ 
  
  export default functions;
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -1,6 +1,53 @@
 import functions from './daily'
-  
+// OCT 15-16 2019
+test('sort numbers in ascending order', () => {
+    expect(functions.sortAscending([40,20,35,3])).toEqual([3,20,35,40]); // Consider the edge cases
+});
+test('sort number in descending order' , () => {
+    expect(functions.sortDescending([40,20,35,3])).toEqual([40,35,20,3]);
+});
+test('filter number >2', () => {
+    expect(functions.filterFunction([1,2,3,4])).toEqual([3,4]);
+});
+test('add all the elems in one array', () => {
+    expect(functions.reduceFunction([1,3,6])).toBe(10);
+});
+test('elem value increased 5 times',() =>{
+    expect(functions.mapFunction([2,4,6])).toEqual([10,20,30]); // Consider the edge cases
+});
+test('slice', () => {    
+    expect(functions.sliceFunction([1,2,3])).toEqual([2,3]);
+    expect(functions.sliceFunction(["grapes","apple","kiwi"])).toEqual(["apple","kiwi"]);
+});
+test('splice', () => {    
+    expect(functions.spliceFunction([1,2,3])).toEqual([1,"splice",2,3]);
+    expect(functions.spliceFunction(["apple","kiwi"])).toEqual(["apple","splice","kiwi"]);
+});
+test('foreach', () =>{
+   expect(functions.forEachFunction([1,2,3])).toEqual([1,2,3]);
+   expect(functions.forEachFunction(["grapes","apple","kiwi"])).toEqual(["grapes","apple","kiwi"]);
+});  
+test('forin',()=>{ 
+    expect(functions.forInLoop({a: "first", b:"second", c:"third"})).toEqual(["first","second","third"]);
+     });
+test('forof',()=>{ 
+    expect(functions.forOfLoop([1,2,3])).toEqual([1,2,3]);
+    expect(functions.forOfLoop(["grapes","apple","kiwi"])).toEqual(["grapes","apple","kiwi"]);
+ });
+      
+test('dowhile',()=>{ 
+    expect(functions.doWhile(5)).toEqual([1,2,3,4,5]);
+     });
+ 
+test('while',()=>{ 
+    expect(functions.whileLoop("parveen")).toBe("neevrap");
+}); 
+test('forloop',()=>{ 
+    expect(functions.basicForLoop(0,5)).toBe(15);
+    expect(functions.basicForLoop(0,10)).toBe(55);
+ });
 
+// OCT 11 2019
 test('email builder from an object/map', () => {
     const name = {fname: "first", lname:"last"};
     expect(functions.makeEmailObject(name)).toEqual("first.last@evolveu.ca");
@@ -9,21 +56,14 @@ test('email builder from an object/map', () => {
 });
 
 
-test('Check 2 parameters', () => {
-    expect(functions.assertEqual("a","a")).toBe("true"); // Consider the edge cases
-    expect(functions.assertEqual("a","b")).toBe("false");
-    expect(functions.assertEqual(1,2)).toBe("false");
-    expect(functions.assertEqual(2,2)).toBe("true");
-    expect(functions.assertEqual("2",2)).toBe("false");
-    expect(functions.assertEqual("This value","This value")).toBe("true");
-});
+
 test('email builder from an array', () => {
     const name = ["first", "last"];
     expect(functions.makeEmailArr(name)).toEqual("first.last@evolveu.ca");
     expect(functions.makeEmailArr(["First", "Last"])).toEqual("first.last@evolveu.ca");
     expect(functions.makeEmailArr(["Bill", "Smith"])).toEqual("bill.smith@evolveu.ca");
 });
-
+//OCt 7
 test('Check 2 parameters', () => {
     expect(functions.assertEqual("a","a")).toBe("true"); // Consider the edge cases
     expect(functions.assertEqual("a","b")).toBe("false");
